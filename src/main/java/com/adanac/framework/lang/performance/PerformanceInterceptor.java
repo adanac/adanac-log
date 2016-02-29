681eae9ab4d9ed489951818743e54869c7bc585a
adanac <adanac@sina.com>
2016-02-29 20:56:57 +0800

    update files

---
+++ src/main/java/com/adanac/framework/lang/performance/PerformanceInterceptor.java
@@ -0,0 +1,180 @@
+package com.adanac.framework.lang.performance;
+
+import java.util.Stack;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+
+import com.adanac.framework.lang.exception.BaseException;
+import com.adanac.framework.lang.util.EventUtils;
+import com.adanac.framework.log.MyLogger;
+import com.adanac.framework.log.MyLoggerFactory;
+import com.bn.framework.lang.dto.PerformanceEventMessage;
+
+/**
+ * 
+ * @author adanac
+ * @version 1.0
+ */
+public class PerformanceInterceptor {
+	protected MyLogger logger;
+
+	protected String loggerName;
+
+	protected ThreadLocal<Stack<PerformanceEventMessage>> threadMsgStack = new ThreadLocal<Stack<PerformanceEventMessage>>();
+
+	public PerformanceInterceptor() {
+		super();
+	}
+
+	/**
+	 * 
+	 * 功能描述: <br>
+	 * spring aop环绕事件，用于计算方法执行时间以及相应的性能事件的属性（包括方法名，父方法名，执行状态，相对于调用链中的第一个方法的执行时间）
+	 * 
+	 * @param pjp 待执行的方法切点
+	 * @return
+	 * @throws Throwable
+	 */
+	public Object aroundProcess(ProceedingJoinPoint pjp) throws Throwable {
+		afterPropertiesSet();
+
+		// 生成新的性能事件以及填充相应信息
+		PerformanceEventMessage me = new PerformanceEventMessage(this.generateFunctionName(pjp));
+		// 父性能事件
+		PerformanceEventMessage parent = this.getTopStackElement();
+		// 此次请求的第一个性能事件
+		PerformanceEventMessage first = this.getFirstStackElement();
+		me.setSequence(this.getStackLength() + 1);
+		if (parent != null) {
+			me.setFunCallerName(parent.getFunName());
+		} else {
+			me.setFunCallerName(PerformanceEventMessage.NAN_CALLER);
+		}
+		if (first != null) {
+			me.setRelativeTime(me.getEventTime().getTime() - first.getEventTime().getTime());
+		} else {
+			me.setRelativeTime(0);
+		}
+
+		// 于方法调用之前，将本事件入栈
+		this.pushToStack(me);
+
+		// 开始执行方法
+		// 执行结果存放容器
+		Object result = null;
+		try {
+			me.begin();
+			result = pjp.proceed();
+		} catch (BaseException baseEx) {
+			me.setStatus(baseEx.getCode());
+			throw baseEx;
+		} catch (Throwable t) {
+			me.setStatus(PerformanceEventMessage.NAN_ERROR_CODE);
+		} finally {
+			// 方法执行完毕，将本事件出栈
+			this.popFromStack();
+			me.end();
+			if (this.logger.isWarnEnabled()) {
+				logger.warn(EventUtils.EVENT_GSON.toJson(me), new Object[] {});
+			}
+		}
+		return result;
+	}
+
+	public void afterPropertiesSet() throws Exception {
+		if (this.loggerName == null) {
+			this.logger = MyLoggerFactory.getLogger(PerformanceInterceptor.class);
+		} else {
+			this.logger = MyLoggerFactory.getLogger(this.loggerName);
+		}
+	}
+
+	/**
+	 * 设置日志名
+	 * 
+	 * @param loggerName 日志名
+	 */
+	public void setLoggerName(String loggerName) {
+		this.loggerName = loggerName;
+	}
+
+	/**
+	 * 
+	 * 将性能事件压入threadLocal级别的栈中
+	 * 
+	 * @param pEventMsg 待push的性能事件
+	 */
+	protected void pushToStack(PerformanceEventMessage pEventMsg) {
+		Stack<PerformanceEventMessage> pStack = this.threadMsgStack.get();
+		if (pStack == null) {
+			pStack = new Stack<PerformanceEventMessage>();
+			this.threadMsgStack.set(pStack);
+		}
+		pStack.push(pEventMsg);
+	}
+
+	/**
+	 * 
+	 * 将性能事件从threadLocal级别的栈中弹出
+	 * 
+	 * @return 弹出的性能事件
+	 */
+	protected PerformanceEventMessage popFromStack() {
+		Stack<PerformanceEventMessage> pStack = this.threadMsgStack.get();
+		if (pStack != null && pStack.size() != 0) {
+			return pStack.pop();
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * 取得threadLocal级别的栈的栈顶元素
+	 * 
+	 * @return 栈顶事件
+	 */
+	protected PerformanceEventMessage getTopStackElement() {
+		Stack<PerformanceEventMessage> pStack = this.threadMsgStack.get();
+		if (pStack != null && pStack.size() != 0) {
+			return pStack.lastElement();
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * 取得threadLocal级别的栈的第一个性能事件（栈底元素）
+	 * 
+	 * @return 栈中的第一个性能事件
+	 */
+	protected PerformanceEventMessage getFirstStackElement() {
+		Stack<PerformanceEventMessage> pStack = this.threadMsgStack.get();
+		if (pStack != null && pStack.size() != 0) {
+			return pStack.firstElement();
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * 取得threadLocal级别的栈的长度
+	 * 
+	 * @return 栈的长度
+	 */
+	protected int getStackLength() {
+		Stack<PerformanceEventMessage> pStack = this.threadMsgStack.get();
+		if (pStack != null) {
+			return pStack.size();
+		}
+		return 0;
+	}
+
+	protected String generateFunctionName(ProceedingJoinPoint pjp) {
+		Signature signature = pjp.getSignature();
+		StringBuffer functionName = new StringBuffer();
+		functionName.append(signature.getDeclaringTypeName() + "#");
+		functionName.append(signature.getName());
+		return functionName.toString();
+	}
+}
---
+++ src/main/java/com/adanac/framework/log/LogConfigManager.java
@@ -0,0 +1,129 @@
+package com.adanac.framework.log;
+
+import java.io.StringReader;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.ibatis.logging.LogException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.xml.sax.InputSource;
+
+import com.adanac.framework.statistics.VersionStatistics;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+
+/**
+ * 
+ * @author adanac
+ * @version 1.0
+ */
+public class LogConfigManager {
+	private static final Logger logger = LoggerFactory.getLogger(LogConfigManager.class);
+
+	private static final String APP_LOG_CONFIG_PATH = "/log.config";
+
+	private static final String GLOBAL_LOG_CONFIG_PATH = "/log.global";
+
+	private static volatile UniconfigNode appConfigNode;
+
+	private static volatile UniconfigNode globalConfigNode;
+
+	private static volatile Map<String, UniconfigNode> globalPieceConfigNodes = new ConcurrentHashMap<String, UniconfigNode>();
+
+	private static UniconfigListener listener = new UniconfigListener() {
+		@Override
+		public void execute(String oldValue, String newValue) {
+			doLogConfigure();
+		}
+	};
+
+	static {
+		SLF4JBridgeHandler.install();
+		VersionStatistics.reportVersion(LogConfigManager.class);
+	}
+
+	public static void init() {
+		doLogConfigure();
+	}
+
+	private static String getGlobalConfigs() {
+		UniconfigClient client = UniconfigClientImpl.getInstance();
+		if (globalConfigNode == null) {
+			globalConfigNode = client.getGlobalConfig(GLOBAL_LOG_CONFIG_PATH);
+			globalConfigNode.sync();
+			globalConfigNode.monitor(listener);
+		}
+		String globalConfigPaths = globalConfigNode.getValue();
+		Map<String, UniconfigNode> newPiecesMap = new ConcurrentHashMap<String, UniconfigNode>();
+		if (globalConfigPaths != null && !globalConfigPaths.trim().equals("")) {
+			String[] piecePaths = globalConfigPaths.split("\\,");
+			for (String piecePath : piecePaths) {
+				UniconfigNode node = globalPieceConfigNodes.get(piecePath);
+				if (node == null) {
+					node = client.getGlobalConfig(piecePath);
+					node.sync();
+					node.monitor(listener);
+				}
+				newPiecesMap.put(piecePath, node);
+			}
+		}
+		for (Map.Entry<String, UniconfigNode> entry : globalPieceConfigNodes.entrySet()) {
+			if (!newPiecesMap.containsKey(entry.getKey())) {
+				entry.getValue().destroy();
+			}
+		}
+		globalPieceConfigNodes = newPiecesMap;
+		StringBuilder sb = new StringBuilder();
+		for (UniconfigNode node : globalPieceConfigNodes.values()) {
+			String value = node.getValue();
+			if (value != null && !"".equals(value.trim())) {
+				sb.append(node.getValue());
+				sb.append("\n");
+			}
+		}
+		String configs = sb.toString();
+		checkGlobalConfigFormat(configs);
+		return configs;
+	}
+
+	protected synchronized static void doLogConfigure() {
+		logger.info("begin do log configure.");
+		UniconfigClient client = UniconfigClientImpl.getInstance();
+		if (appConfigNode == null) {
+			appConfigNode = client.getConfig(APP_LOG_CONFIG_PATH);
+			appConfigNode.sync();
+			appConfigNode.monitor(listener);
+		}
+		String appConfig = appConfigNode.getValue();
+		String globalConfigs = getGlobalConfigs();
+		if (appConfig != null && !appConfig.equals("")) {
+			String logConfig = combineLogConfig(appConfig, globalConfigs);
+			StringReader reader = new StringReader(logConfig);
+			InputSource is = new InputSource(reader);
+			LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+			loggerContext.reset();
+			JoranConfigurator joranConfigurator = new JoranConfigurator();
+			joranConfigurator.setContext(loggerContext);
+			try {
+				joranConfigurator.doConfigure(is);
+			} catch (JoranException ex) {
+				logger.warn("Exception:", ex);
+			}
+		}
+	}
+
+	private static String combineLogConfig(String appConfig, String globalConfigs) {
+		return appConfig.split("</configuration>")[0] + "\n" + globalConfigs + "\n" + "</configuration>";
+	}
+
+	private static void checkGlobalConfigFormat(String globalLogConfig) {
+		if (globalLogConfig.contains("<configuration") || globalLogConfig.contains("</configuration>")
+				|| globalLogConfig.contains("<root") || globalLogConfig.contains("</root>")) {
+			throw new LogException("'<configuration>','<root>' can't exist in 'log.global'.");
+		}
+	}
+}
